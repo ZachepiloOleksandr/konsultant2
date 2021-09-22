b768d96f9c6cf38937b2d58d2d497f079f0d7e74
Your Name <you@example.com>
2021-09-22 13:02:47 +0300

    do ok

--- main.js
+++ main.js
@@ -5,32 +5,36 @@ const inteligeceHandler = (event) => {
 
     inteligece_text.innerHTML = event.target.value;
     event.target.focus();
-    console.log('focus');
 };
 const criticalHandler = (event) => {
     const critical_text = document.getElementById("critical_text");
 
     critical_text.innerHTML = event.target.value;
+    event.target.focus();
 };
 const initiateHandler = (event) => {
     const initiate_text = document.getElementById("initiate_text");
 
     initiate_text.innerHTML = event.target.value;
+    event.target.focus();
 };
 const ustupchivostHandler = (event) => {
     const ustupchivost_text = document.getElementById("ustupchivost_text");
 
     ustupchivost_text.innerHTML = event.target.value;
+    event.target.focus();
 };
 const friendlyestHandler = (event) => {
     const ifriendlyest_text = document.getElementById("friendlyest_text");
 
     ifriendlyest_text.innerHTML = event.target.value;
+    event.target.focus();
 };
 const emotionsHandler = (event) => {
     const emotions_text = document.getElementById("emotions_text");
 
     emotions_text.innerHTML = event.target.value;
+    event.target.focus();
 };
 
 
@@ -41,6 +45,12 @@ const buttonClick = () => {
     const ustupchivost_input = document.getElementById("ustupchivost_input");
     const friendlyest_input = document.getElementById("friendlyest_input");
     const emotions_input = document.getElementById("emotions_input");
+    const vspluvashka = document.getElementById("vspluvashka");
+    const res1 = document.getElementById("res1");
+    const res2 = document.getElementById("res2");
+    const res3 = document.getElementById("res3");
+
+    vspluvashka.style.display = 'block';
 
     const _int = inteligece_input.value;
     const _crt = critical_input.value;
@@ -103,79 +113,68 @@ const buttonClick = () => {
     //     console.log(status_texts[kolich[i]] + bokvu_texts[i][kolich[i]]);
     // }
 
+    let stroka = "";
+
     poryadok.forEach((element, index) => {
         let num = poryadok[index - 1]?.num + 1 || 1;
 
         if(index !== 0) {
-
+            console.log(num);
             if (element.value === poryadok[0].value) {
                 num = poryadok[0].num;
                 poryadok[index].num = num;
             }
-
+console.log(num);
 
             if (element.key !== poryadok[1].key &&  element.value === poryadok[1].value) {
                 num = poryadok[1].num;
                 poryadok[index].num = num;
             }
-
-console.log('-----');
 console.log(num);
 
             if (element.key !== poryadok[2].key && element.value === poryadok[2].value) {
-                num = poryadok[2].num;
+                num = poryadok[2].num || num;
                 poryadok[index].num = num;
             }
-// console.log(num);
-//             if (element.key !== poryadok[3].key && element.value === poryadok[3].value) {
-//                 num = poryadok[3].num;
-//                 poryadok[index].num = num;
-//             }
+
         }
 
 
         poryadok[index].num = num;
         console.log(element.key, element.value);
+        console.log(status_texts[num] , bokvu_texts[element.key][num]);
         console.log(status_texts[num] + bokvu_texts[element.key][num]);
-
+        stroka = stroka + status_texts[num] + bokvu_texts[element.key][num];
     });
+
+    res1.innerHTML = stroka;
+
  
-    // let stroka =  status_texts[kolich.E] + E_texts[kolich.E];
+    poryadok.forEach((element, index) => {
+        if (element.num === 1) {
+            // stroka = stroka + '\n Що головне для клиэнта? \n' + golovne_texts[element.key];
+            res2.innerHTML = golovne_texts[element.key];
 
-        // if(kolich["E"]===1) {
-        //     stroka = status_texts.z
-        // }
-        // if(kolich["E"]===2) {
-        //     stroka = status_texts.vc + E_texts.vc;
-        // }
-        // if(kolich["E"]===3) {
-        //     stroka = status_texts.ch
-        // }
-        // if(kolich["E"]===4) {
-        //     stroka = status_texts.n
-        // }
+        }
+    });
+    poryadok.forEach((element, index) => {
+        if (element.num === 1) {
+            // stroka = stroka + '\n Дії консультанта \n' + dii_texts[element.key];
+            res3.innerHTML = dii_texts[element.key];
 
+        }
+    });
 
-    let status = {
-        "E": 0,
-        "T": 0,
-        "G": 0,
-        "A": 0,
-    };
-
-
-// console.log('kolich', kolich);
-// console.log('procentu', procentu);
-console.log("poryadok", poryadok);
-
-// console.log(poryadok[0].value);
-// console.log(poryadok[0].key);
+    // vspluvashka.innerHTML = stroka;
 
 }
 
 
+const okClick = () => {
+    const vspluvashka = document.getElementById("vspluvashka");
 
-
+    vspluvashka.style.display = 'none';
+};
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -186,6 +185,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const friendlyest_input = document.getElementById("friendlyest_input");
     const emotions_input = document.getElementById("emotions_input");
     const button = document.getElementById("button");
+    const ok = document.getElementById("ok");
 
     inteligece_input.addEventListener('input', inteligeceHandler)
     critical_input.addEventListener('input', criticalHandler)
@@ -193,7 +193,8 @@ document.addEventListener("DOMContentLoaded", () => {
     ustupchivost_input.addEventListener('input', ustupchivostHandler)
     friendlyest_input.addEventListener('input', friendlyestHandler)
     emotions_input.addEventListener('input', emotionsHandler)
-    button.addEventListener('click', buttonClick)
+    button.addEventListener('click', buttonClick);
+    ok.addEventListener('click', okClick);
 });
 
 
@@ -227,6 +228,19 @@ const bokvu_texts = {
 const status_texts = {
     "1": "",
     "2": "В цілому",
-    "3": "Частково",
+    "3": "Майже не",
     "4": "Не",
+};
+
+const golovne_texts = {
+    "A": "Отримання максимальної вигоди за мінімальні витрати.",
+    "T": "100% результат в найкоротші терміни.",
+    "G": "Привітні і дружні відносини в процесі ведення справи.",
+    "E": "Видимість бурхливої та емоційної деятеьлності в роботі.",
+};
+const dii_texts = {
+    "A": "Чітко і детально викладати свої думки. Говорити повільно. Надавати точні дані і вміти їх довести. Намалювати клієнтові повну картину. Бути логічним. Не підштовхувати клієнта до швидких рішень. Не намагатися домінувати.",
+    "T": "Ретельно готуватися до зустрічі. Бути лаконічним. Бути енергійним і швидко переходити до справи. Триматися діловито і професійно. Запропонувати можливість вибору. Бути наполегливим. Наголосити на невідкладності і актуальності швидких дій прямо зараз.",
+    "G": "Говорити повільно. Триматися дружелюбно. Ставити особисті питання. Бути привітним в спілкуванні. Пропонувати свою допомогу. Підкреслювати своє бажання допомогти. Виявляти особисту зацікавленість у справі.",
+    "E": "Надихати до дії. Посилатися на думку інших людей. Давати можливість співрозмовнику виговоритися. Триматися впевнено. Впевнено переходити до справи. Говорити ясно і однозначно.",
 };
